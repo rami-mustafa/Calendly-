f2e6e82b498ce579827776862c844cd57cc2d844
Rami Mustafa <ramimustafa77@gmail.com>
2023-09-17 21:30:45 +0200

    First comment

---
+++ Calendly/Calendly/CalendlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendlyApp.swift
+//  Calendly
+//
+//  Created by Ghaiath Alhereh on 17.09.23.
+//
+
+import SwiftUI
+
+@main
+struct CalendlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
