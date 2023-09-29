cdc7b9c24c29c94231b5cf64dbb7e06bb970f213
Rami Mustafa <ramimustafa77@gmail.com>
2023-09-29 22:49:19 +0200

    printing days on the screen

--- Calendly/Calendly/ContentView.swift
+++ Calendly/Calendly/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     let days = ["Sun" , "Mon" , "Tue" , "Wed" , "Thu" , "Fri" ,"Sat"]
-    @State var selectedMonth = 0
+    @State var selectedMonth = 1
     var body: some View {
         VStack {
             Image("rami")
@@ -65,18 +65,23 @@ struct ContentView: View {
                 }
                 
                 LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7)) {
-                    
+                    ForEach(fetchDara()) { value in
+                        Text("\(value.day)")
+                    }
                 }
             }
             
         }
     }
     
-    //    func fetchDara() -> [CalendarData] {
-    //        let Calendar = Calendar.current
-    //        let cureentMonth = fetchSelectedMonth()
-    //
-    //    }
+    func fetchDara() -> [CalendarDate] {
+        let Calendar = Calendar.current
+        let cureentMonth = fetchSelectedMonth()
+        
+        var datesOfMonth = cureentMonth.datesOfMonth().map({CalendarDate(day: Calendar.component(.day, from: $0), date: $0)})
+        
+        return datesOfMonth
+    }
     
     
     func fetchSelectedMonth() -> Date {
@@ -85,10 +90,10 @@ struct ContentView: View {
         return month!
     }
     
-    struct CalendarData : Identifiable {
+    struct CalendarDate : Identifiable {
         let id = UUID()
         var day: Int
-        var data: Date
+        var date: Date
     }
 }
 
@@ -104,21 +109,21 @@ extension Date {
         let calendar = Calendar.current
         let currentMonth = calendar.component(.month, from: self)
         let currentYear = calendar.component(.year, from: self)
-       
+        
         var startDateComponents = DateComponents()
         startDateComponents.year = currentYear
         startDateComponents.month = currentMonth
         startDateComponents.day = 1
         let startDate = calendar.date(from: startDateComponents)!
-       
+        
         var endDateComponents = DateComponents()
         endDateComponents.month = 1
         endDateComponents.day = -1
         let endDate = calendar.date (byAdding: endDateComponents, to: startDate)!
-       
+        
         var dates: [Date] = []
         var currentDate = startDate
-       
+        
         while currentDate <= endDate {
             dates.append (currentDate)
             currentDate = calendar.date (byAdding: .day, value: 1, to: currentDate)!
